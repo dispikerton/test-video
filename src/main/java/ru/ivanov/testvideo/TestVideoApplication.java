cb2a768196fc15a044babc1d5100b05c3698027c
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-04-06 10:19:51 +0400

    Initial commit

---
+++ src/main/java/ru/ivanov/testvideo/TestVideoApplication.java
@@ -0,0 +1,13 @@
+package ru.ivanov.testvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVideoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestVideoApplication.class, args);
+  }
+
+}
