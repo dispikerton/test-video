202fa84231017d3b675dd5d7be1289d174bf654d
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-04-17 11:28:24 +0400

    Таймаут по умолчанию

--- src/test/java/ru/ivanov/testvideo/TestTimeout.java
+++ src/test/java/ru/ivanov/testvideo/TestTimeout.java
@@ -1,16 +1,14 @@
 package ru.ivanov.testvideo;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class TestTimeout {
 
     @Test
-    @Timeout(1)
     void testTimeout() throws InterruptedException {
-        Thread.sleep(1100);
+        Thread.sleep(100);
         assertTrue(true);
     }
 }
