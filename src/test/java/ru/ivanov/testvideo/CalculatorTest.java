011d581ce2462ecbfcdf7e58a6131abd5fadc6b1
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-05-03 11:57:16 +0400

    Ещё тесты

--- src/test/java/ru/ivanov/testvideo/CalculatorTest.java
+++ src/test/java/ru/ivanov/testvideo/CalculatorTest.java
@@ -11,4 +11,17 @@ class CalculatorTest {
     int multiply = calculator.multiply(5, 4);
     Assertions.assertEquals(20, multiply);
   }
+
+  @Test
+  void testDivide() {
+    Calculator calculator = new Calculator();
+    int divide = calculator.divide(20, 4);
+    Assertions.assertEquals(5, divide);
+  }
+
+  @Test
+  void testDivideByZero() {
+    Calculator calculator = new Calculator();
+    Assertions.assertThrows(ArithmeticException.class, () -> calculator.divide(20, 0));
+  }
 }
